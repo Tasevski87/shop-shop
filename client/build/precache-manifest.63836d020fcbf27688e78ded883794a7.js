50783734d80f316fdda2475cdde69e924497d536
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-03-30 10:47:55 -0400

    idb

---
+++ client/build/precache-manifest.63836d020fcbf27688e78ded883794a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234e53b845aa760640a9ddf95b237cdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99ca1bee41038596bb4d",
+    "url": "/static/css/main.c125594a.chunk.css"
+  },
+  {
+    "revision": "68edb3b3cf136beaf15f",
+    "url": "/static/js/2.154e1d96.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.154e1d96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99ca1bee41038596bb4d",
+    "url": "/static/js/main.b5a63931.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
     const [, dispatch] = useStoreContext();
@@ -10,7 +11,9 @@ const CartItem = ({ item }) => {
             type: REMOVE_FROM_CART,
             _id: item._id
         });
+        idbPromise('cart', 'delete', { ...item });
     };
+
     const onChange = (e) => {
         const value = e.target.value;
 
@@ -19,46 +22,51 @@ const CartItem = ({ item }) => {
                 type: REMOVE_FROM_CART,
                 _id: item._id
             });
+
+            idbPromise('cart', 'delete', { ...item });
         } else {
             dispatch({
                 type: UPDATE_CART_QUANTITY,
                 _id: item._id,
                 purchaseQuantity: parseInt(value)
             });
+
+            idbPromise('cart', 'put', { ...item, purchaseQuantity: parseInt(value) });
         }
-    };
+    }
 
-    return (
-        <div className="flex-row">
-            <div>
-                <img
-                    src={`/images/${item.image}`}
-                    alt=""
-                />
-            </div>
-            <div>
-                <div>{item.name}, ${item.price}</div>
+
+        return (
+            <div className="flex-row">
                 <div>
-                    <span>Qty:</span>
-                    <input
-                        type="number"
-                        placeholder="1"
-                        value={item.purchaseQuantity}
-                        onChange={onChange}
-
+                    <img
+                        src={`/images/${item.image}`}
+                        alt=""
                     />
-                    <span
-                        role="img"
-                        aria-label="trash"
-                        onClick={() => removeFromCart(item)}
+                </div>
+                <div>
+                    <div>{item.name}, ${item.price}</div>
+                    <div>
+                        <span>Qty:</span>
+                        <input
+                            type="number"
+                            placeholder="1"
+                            value={item.purchaseQuantity}
+                            onChange={onChange}
 
-                    >
-                        🗑️
-                    </span>
+                        />
+                        <span
+                            role="img"
+                            aria-label="trash"
+                            onClick={() => removeFromCart(item)}
+
+                        >
+                            🗑️
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
 
-export default CartItem;
+    export default CartItem;
